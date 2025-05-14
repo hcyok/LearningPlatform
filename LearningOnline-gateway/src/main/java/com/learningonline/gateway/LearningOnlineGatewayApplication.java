d4f3bd8354cccdab58d81102772de176751bd60f
yhc <3020696716@qq.com>
2025-05-14 21:00:18 +0800

    网关工程配置

---
+++ LearningOnline-gateway/src/main/java/com/learningonline/gateway/LearningOnlineGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.learningonline.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningOnlineGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearningOnlineGatewayApplication.class, args);
+    }
+
+}
---
+++ LearningOnline-gateway/src/test/java/com/learningonline/gateway/LearningOnlineGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learningonline.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningOnlineGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
